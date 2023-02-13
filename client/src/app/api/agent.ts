5edd80aba8c7e9b0e29021901cf45a9a088aec3c
AlinaRusanova <104161655+AlinaRusanova@users.noreply.github.com>
2023-02-13 11:54:20 +0200

    added basket

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 const sleep = () => new Promise(resolve => setTimeout(resolve, 300));
 
 axios.defaults.baseURL='http://localhost:7000/api/';
+axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
@@ -27,6 +28,12 @@ const requests = {
     }).then(responseBody)
 }
 
+// const Admin = {
+//     createProduct: (product: any) => requests.postForm('products', createFormData(product)),
+//     updateProduct: (product: any) => requests.putForm('products', createFormData(product)),
+//     deleteProduct: (id: number) => requests.delete(`products/${id}`)
+// }
+
 const Catalog = {
      list: (/*params: URLSearchParams*/) => requests.get('products'/*, params*/),
     details: (id: number) => requests.get(`products/${id}`),
@@ -41,10 +48,34 @@ const TestErrors = {
     getValidationError: () => requests.get('buggy/validation-error'),
 }
 
+const Basket = {
+    get: () => requests.get('basket'),
+    addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+    removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`)
+}
+
+// const Account = {
+//     login: (values: any) => requests.post('account/login', values),
+//     register: (values: any) => requests.post('account/register', values),
+//     currentUser: () => requests.get('account/currentUser'),
+//     fetchAddress: () => requests.get('account/savedAddress')
+// }
+
+// const Orders = {
+//     list: () => requests.get('orders'),
+//     fetch: (id: number) => requests.get(`orders/${id}`),
+//     create: (values: any) => requests.post('orders', values)
+// }
+
+// const Payments = {
+//     createPaymentIntent: () => requests.post('payments', {})
+// }
+
+
 const agent = {
     Catalog,
     TestErrors,
-    // Basket,
+    Basket,
     // Account,
     // Orders,
     // Payments,
